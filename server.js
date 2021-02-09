67ff784c951fe90dc3f66c42946bc6f74021ff59
Fernando Aguilar <FernandoAguilar984@gmail.com>
2021-02-09 07:50:06 -0600

    Fix local environment to run json-server with json file from express

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 const jsonServer = require('json-server');
 const app = express();
+
+app.use(cors())
 
 app.use(express.static(__dirname + '/dist/cvtest-fam'));
 
